3b475af04e68adb4afd6fd1d352b7c6eb4d953c9
Nawal <Nawalir.16@gmail.com>
2024-02-12 12:17:10 +0300

    Handle Exception

---
+++ Exception.py
@@ -0,0 +1,22 @@
+'''
+Below you have a code that raises an exception , using what you learned do the following:
+Find what type of exception is raised.
+   - Hanlde the exception in try..except
+   - If operation successful , print "the operation is successful"
+   - if operation fails, handle the specific exception that is raised , and print a relevant message.
+'''
+
+try:
+    def additoin(x, y):
+        x = 10
+        y = 20
+        print("Addition:", x + y)
+
+    additoin(10, 20)
+
+except NameError:
+    print("An error occurred")
+except Exception as e:
+    print(e.__class__)
+else:
+    print("The operation is successful")
