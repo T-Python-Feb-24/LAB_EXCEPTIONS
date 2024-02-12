4ac4f33ba7a1da120bef05b7a3befdc18cfa9bd2
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-12 14:38:20 +0300

    done.

--- bonus.py
+++ bonus.py
@@ -8,29 +8,27 @@ def fahrenheit_to_celsius(fahrenheit:int)-> float:
 
 def main():
     """This function will raise exeptions and warn the user of the error."""
-    while True:
-        try:
-            Input = input("Enter the temerature follwoed by measuring unit:")
-            temp, unit = Input.split()
-            temp = float(temp)
 
-            if unit.upper() == "C":
-                conv_temp = celsius_to_fahrenheit(temp)
-                print("Temperature in fahrenheit is: ", round(conv_temp, 2), "F")
+    Input = input("Enter the temerature follwoed by measuring unit:")
+    temp, unit = Input.split()
+    temp = float(temp)
+
+    if unit.upper() == "C":
+        conv_temp = celsius_to_fahrenheit(temp)
+        print("Temperature in fahrenheit is: ", round(conv_temp, 2), "F")
             
-            elif unit.upper() == "F":
-                conv_temp = fahrenheit_to_celsius(temp)
-                print("Temperature in celsius is: ", round(conv_temp, 2), "C")
-            else:
-                raise TypeError("Invalid input, please use C or F to enter input.")
+    elif unit.upper() == "F":
+        conv_temp = fahrenheit_to_celsius(temp)
+        print("Temperature in celsius is: ", round(conv_temp, 2), "C")
+    else:
+        raise TypeError("Invalid input, please use C or F to enter input.")
             
-        except ValueError:
-            print("Invalid input, please use real numbers to input the temperature.")
-        except TypeError as e:
-            print(e)
-    
-if __name__ == "__main__":
+try:
     main()
+except ValueError:
+    print("Invalid input, please use real numbers to input the temperature.")
+except TypeError as e:
+    print(e)
             
 
 
