2a1a4d310ac6771fdc7257cbecf91842c4321121
rahaf <rahaf09876509@gmail.com>
2024-02-12 20:42:39 +0300

    NEW

--- Ex_Bouns.py
+++ Ex_Bouns.py
@@ -6,26 +6,23 @@ def fahrenheit1(fahrenheit):
     return(fahrenheit - 32) * 5/9
     
 
-def main():
+
     while True:
         try:
             user_input = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
-            temperature = float(temperature)
+            temperature = float("temperature :" )
 
             if user_input.upper() == 'C':
-                converted_temperature = celsius1(temperature)
-                converted_unit = 'Fahrenheit'
+                converted_temperature = round(celsius1(temperature),2)
+                print(f"temperature in fahrenheit1 is {converted_temperature}F")
             elif user_input.upper() == 'F':
-                converted_temperature = fahrenheit1(temperature)
-                converted_unit = 'Celsius'
+                converted_temperature = round(fahrenheit1(temperature),2)
+                print(f"temperature in celsius is {converted_temperature}C")
             else:
-                raise TypeError("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
-
-            print(f"The converted temperature is {converted_temperature} {converted_unit}.")
-            break
+                raise TypeError(f"the user input{user_input}is invalid. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
 
         except ValueError:
             print("Invalid temperature value. Please try again.")
         except TypeError as e:
-            print(str(e))
-main()
+            print(e)
+
