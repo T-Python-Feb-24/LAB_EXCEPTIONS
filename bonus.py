f3b3f20dc9a3319721ea918f47358d06b221bae7
jmailnaif <jamailotb15@gmail.com>
2024-02-12 23:35:54 +0300

    lab exceptions

---
+++ bonus.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit =(celsius*9/5)+32
+    return
+fahrenheit 
+def fahrenheit = (fahrenheit - 32)*5/9
+return 
+celsius
+def main()
+    user_input = input("enter a temperatureand its unit (e.g.,'25c' or '77f'):")
+try:
+    temperature = user_input.split()
+    temperature = float(temperature)
+    if unit.upper()=='c'
+       fahrenheit_temp =celsius_to_fahrenheit(temperature)
+       print(f"the temperature in fahrenheit")
---
+++ exceptions.py
@@ -0,0 +1,9 @@
+def addiotoin(x, y):
+    X = 10
+    y= 20
+print( "addition:" , X + b )
+try:
+    addiotoin(10 , 20)
+    print("operation is successful")
+except NameError
+    print ("  operaation complete ")
