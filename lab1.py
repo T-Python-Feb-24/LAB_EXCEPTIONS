18b28bf9bb49648c72faecc9da44dfc4768d3bfe
lama20001117 <lamayehya2000@gmail.com>
2024-02-14 13:27:43 +0300

    update

--- lab1.py
+++ lab1.py
@@ -1,12 +1,13 @@
 def additoin(x, y):
-   try:
      x = 10
      y = 20
      print("Addition:", x + b )
-     print("the operation is successful" )
-   except NameError:
+try:
+    additoin(10, 20)
+except NameError:
       print("the operation is fails")
-   except Exception as e:
+except Exception as e:
      print(e.__class__)
-additoin(10, 20)
+else:
+    print("the operation is successful" )
 
